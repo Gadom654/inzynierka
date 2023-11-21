0d45a6ac842b16ad9ce9fcd635738408c30f992a
Dominik Gadomski <dominikgadomski654@gmail.com>
2023-11-21 18:04:28 +0100

    CI/CD configuration

--- parsing.sh
+++ parsing.sh
@@ -6,9 +6,6 @@ AMI_BLUE_ID=$(terraform show -json | jq -r '.values.root_module.resources[] | se
 # Extract the AMI ID using jq
 AMI_ID=$(jq -r '.builds[0].artifact_id' manifest.json | cut -d: -f2)
 
-# Print the AMI ID (optional)
-echo "Extracted AMI ID: $AMI_BLUE_ID"
-
 # Export the AMI ID as an environment variable
-export TF_VAR_green_ami_id="$AMI_ID"
-export TF_VAR_blue_ami_id="$AMI_BLUE_ID"
+echo "green_ami_id = "$AMI_ID"" >> terraform.auto.tfvars
+echo "blue_ami_id = "$AMI_BLUE_ID"" >> terraform.auto.tfvars
